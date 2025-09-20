328e6aa7344d17da0fac612dc2c1f69e1367e293
lingbopro <ilovemc@zohomail.cn>
2025-09-20 20:42:14 +0800

    GUI components

--- common/src/main/java/me/lingbopro/licensechecker/gui/LicenseListScreen.java
+++ common/src/main/java/me/lingbopro/licensechecker/gui/LicenseListScreen.java
@@ -1,14 +1,37 @@
 package me.lingbopro.licensechecker.gui;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class LicenseListScreen extends Screen {
+    private final Screen parent;
+
     public LicenseListScreen(Screen parent) {
         super(Component.translatable("config.license_checker.title"));
+        this.parent = parent;
     }
 
-    public LicenseListScreen() {
-        this(null);
+    @Override
+    protected void init() {
+        super.init();
+
+        Button backButton = Button.builder(Component.translatable("gui.back"), button -> this.onClose()).bounds(this.width - 100, 15, 80, 20)
+        .build();
+
+        this.addRenderableWidget(backButton);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        guiGraphics.drawString(this.font, this.title, 20, 20, 0xFFFFFFFF);
+    }
+
+    @Override
+    public void onClose() {
+        Minecraft.getInstance().setScreen(this.parent);
     }
 }
